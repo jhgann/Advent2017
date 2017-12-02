9ecd85323bf10a47c8cb916838088d13c66a9d97
Josh Gann <jhgann@gmail.com>
2017-12-02 20:22:31 +0100

    Advent day 2

---
+++ advent2017-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace advent2017_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var splitLines = System.IO.File.ReadLines("input.txt").Select(line => line.Split('\t'));
+            var numberLists = splitLines.Select(line => 
+                line.Select(item => 
+                    int.Parse(item)).ToList()).ToList();
+
+            var checksum = 0;
+            checksum = numberLists.Sum(nums => FindDivisibleValue(nums));
+
+            Console.WriteLine(checksum);
+        }
+
+        static int FindDivisibleValue(List<int> nums) {
+            int divisible = 0;
+            nums = nums.OrderByDescending(x => x).ToList();
+            nums.ForEach(x => {
+                //probably a cleaner way to do this.
+                int? z = nums.FirstOrDefault(y => x!=y && x%y == 0);
+                if (z != null && z.Value != 0) {
+                    divisible = x/z.Value;
+                }
+            });
+            return divisible;
+
+        }
+    }
+}
